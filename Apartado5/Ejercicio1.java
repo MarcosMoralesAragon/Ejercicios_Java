2c8926772e7f37c0bc9d1d769e31b9090c307fd4
Marcos <marcosmoralesaragon@gmail.com>
2020-11-03 10:18:50 +0100

    Subida de Ejercicio 1 y 4 del Apartado 5

---
+++ Apartado5/Ejercicio1.java
@@ -0,0 +1,15 @@
+/**
+ * Muestra los primeros números múltiplos de 5 de 0 a 100
+ *
+ * @author MarcosMoralesAragon
+ */
+public class Ejercicio1 { 
+  public static void main(String[] args) {
+   
+   int i = 0;
+   
+   for (i = 0; i <= 100; i= i + 5 ) {
+     System.out.println(i);
+    }
+	}
+}
---
+++ Apartado5/Ejercicio4.java
@@ -0,0 +1,15 @@
+/**
+ * Contar del 320 al 160 contando de 20 en 20 hacia atras
+ *
+ * @author MarcosMoralesAragon
+ */
+public class Ejercicio4 { 
+  public static void main(String[] args) {
+   
+  int  i = 0;
+   
+   for (i = 320 ; i >= 160; i = i -20){
+     System.out.println(i);
+    }
+	}
+}
